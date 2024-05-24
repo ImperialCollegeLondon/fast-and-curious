84c3fa2499d1773b101a2180cd208ca02d634200
coolernato <cmcooling@gmail.com>
2024-05-24 14:00:23 +0100

    Updated intefrace with Uncertain Cuboids

--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ from testing_resources.common_resources import ProjectResults, print_minor_divis
 
 def run_tests():
     project_results = []
-    project_results.append(test_project_primes(print_project_level_output=False))
+    project_results.append(test_primes(print_project_level_output=False))
     project_results.append(test_uncertain_cuboids(print_project_level_output=False))
 
     total_points = sum([project.points for project in project_results])
@@ -14,21 +14,21 @@ def run_tests():
     print(f'Total Points: {format_number(total_points)}')
 
 
-def test_project_primes(print_project_level_output=True):
+def test_primes(print_project_level_output=True):
     from testing_resources.primes import primes_run_case
     from base.primes import write_primes as base_function
     from sample_solution.primes import write_primes as sample_solution_function
     from student.primes import write_primes as student_function
 
-    project1_results = ProjectResults('Primes', 'Write primes under n to a specified file.')
+    project_results = ProjectResults('Primes', 'Write primes under n to a specified file.')
 
-    project1_results.add_case(primes_run_case(base_function, sample_solution_function, student_function, 1000, '1,000', 'testing_resources/reference_primes_1k.txt'))
-    project1_results.add_case(primes_run_case(base_function, sample_solution_function, student_function, 10000, '10,000', 'testing_resources/reference_primes_10k.txt'))
+    project_results.add_case(primes_run_case(base_function, sample_solution_function, student_function, 1000, '1,000', 'testing_resources/reference_primes_1k.txt'))
+    project_results.add_case(primes_run_case(base_function, sample_solution_function, student_function, 10000, '10,000', 'testing_resources/reference_primes_10k.txt'))
 
     if print_project_level_output:
-        project1_results.print_results()
+        project_results.print_results()
 
-    return project1_results
+    return project_results
 
 def test_uncertain_cuboids(print_project_level_output=True):
     from testing_resources.uncertain_cuboids import uncertain_cuboids_run_case
@@ -36,19 +36,19 @@ def test_uncertain_cuboids(print_project_level_output=True):
     from sample_solution.uncertain_cuboids import calculate_uncertain_cuboid_statistics as sample_solution_function
     from student.uncertain_cuboids import calculate_uncertain_cuboid_statistics as student_function
 
-    project1_results = ProjectResults('Uncertain Cuboids', 'Calculate the mean and standard deviation of the volume of cuboids with uncertain dimensions.')
+    project_results = ProjectResults('Uncertain Cuboids', 'Calculate the mean and standard deviation of the volume of cuboids with uncertain dimensions.')
 
-    project1_results.add_case(uncertain_cuboids_run_case(base_function, sample_solution_function, student_function, 1000000, '1,000,000', 10, 5, 3, 0.5, 0.2, 0.1, (149.99, 150.01), (3.11, 3.15)))
+    project_results.add_case(uncertain_cuboids_run_case(base_function, sample_solution_function, student_function, 1000000, '1,000,000', 10, 5, 3, 0.5, 0.2, 0.1, (149.99, 150.01), (3.11, 3.15)))
 
-    project1_results.add_case(uncertain_cuboids_run_case(base_function, sample_solution_function, student_function, 1000000, '1,000,000', 4, 5, 2, 0, 0, 0, (39.999, 40.001), (0, 0.0001)))
+    project_results.add_case(uncertain_cuboids_run_case(base_function, sample_solution_function, student_function, 1000000, '1,000,000', 4, 5, 2, 0, 0, 0, (39.999, 40.001), (0, 0.0001)))
 
-    project1_results.add_case(uncertain_cuboids_run_case(base_function, sample_solution_function, student_function, 1000000, '1,000,000', 3, 10, 2, 1, 0, 0, (59, 61), (5.75, 5.78)))
+    project_results.add_case(uncertain_cuboids_run_case(base_function, sample_solution_function, student_function, 1000000, '1,000,000', 3, 10, 2, 1, 0, 0, (59, 61), (5.75, 5.78)))
 
     if print_project_level_output:
-        project1_results.print_results()
+        project_results.print_results()
 
-
+    return project_results
 
 
 if __name__ == '__main__':
-    test_uncertain_cuboids()
+    run_tests()
