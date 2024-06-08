38b8a60bf528238dd9a5390efa3efa07717003c4
Jianliang Gao <j.gao@users.noreply.github.com>
2024-06-08 08:34:24 +0100

    added largest inscribed triangle code

---
+++ base/largest_inscribed_triangle.py
@@ -0,0 +1,84 @@
+# student solution
+import math
+import numpy as np
+def largest_triangle_proof(n, radius):
+    """
+    Prove the pattern of largest area of a triangle that can be 
+    inscribed inside a give circle. 
+    
+    This function returns the height of largest area of a triangle 
+    that can be inscribed inside a circle with the given radius, 
+    and base of the triangle is the diameter of the circle
+
+    Args:
+      radius: radius of the circle
+      n: gradient
+
+    Returns:
+      height of the largest triangle
+    """
+    # set the centre of the circle to (0,0)
+    origin_x = 0.0
+    origin_y = 0.0
+
+    #The lower this value the higher quality the circle is with more points generated
+    stepSize = 180/n
+
+    #Generated vertices
+    positions = []
+
+    triangle_area = []
+
+    t = 0.0
+    base_length = 2 * radius
+    # while t <= 0.5 * math.pi: #using radian
+    while t<=90: # using degrees
+        radian_t = math.radians(t)
+        h = radius * math.sin(radian_t)
+        tmp_area = 0.5 * base_length * h
+        if len(triangle_area) == 0:
+            triangle_area.append(tmp_area)
+            positions.append((radius * math.cos(radian_t) + origin_x, radius * math.sin(radian_t) + origin_y))
+        elif tmp_area > max(triangle_area):
+            positions.append((radius * math.cos(radian_t) + origin_x, radius * math.sin(radian_t) + origin_y))
+            triangle_area.append(tmp_area)
+        t += stepSize
+    
+    largest_area_idx = triangle_area.index(max(triangle_area))
+
+    return(positions[largest_area_idx][1])
+
+def largest_triangle_area(radius, h, n):
+    """
+    Returns the largest area of a triangle that can be inscribed inside a
+    circle with the given radius.
+
+    Args:
+      radius: radius of the circle
+      h: the starting height of triangle found after the proof of max area based on diameter as the base of triangle
+      n: gradient
+
+    Returns:
+      Area of the largest inscribed triangle
+    """
+    # The largest triangle is an equilateral triangle
+    largest_A = []
+    step = radius / n
+    
+    for d in np.arange(0, radius, step):
+        base_length = 2 * math.sqrt(radius**2 - d**2)
+        area = 0.5 * base_length * (d + h)
+        if len(largest_A) == 0:
+            largest_A.append(area)
+        elif area>=max(largest_A):
+            largest_A.append(area)
+    return max(largest_A)
+
+# Example usage
+import time
+#radius = 50
+#t0 = time.time()
+#height = largest_triangle_proof(500, radius)
+#max_area = largest_triangle_area(radius, height, 100)
+#t1 = time.time()
+#print(f"The largest area of a triangle inscribed in a circle with radius {radius} is {max_area:.2f} \n Runtime was {t1-t0:.5f}")
---
+++ sample_solution/largest_inscribed_triangle.py
@@ -0,0 +1,83 @@
+# optimised solution
+import math
+import numpy as np
+def largest_triangle_proof(n, radius):
+    """
+    Prove the pattern of largest area of a triangle that can be 
+    inscribed inside a give circle. 
+    
+    This function returns the height of largest area of a triangle 
+    that can be inscribed inside a circle with the given radius, 
+    and base of the triangle is the diameter of the circle
+
+    by Dr Liam Gao (RCDS) June 2024 
+
+    Args:
+      radius: radius of the circle
+      n: gradient
+
+    Returns:
+      height of the largest triangle
+    """
+    
+    # set the centre of the circle to (0,0)
+    origin_x = 0.0
+    origin_y = 0.0
+
+    #The lower this value the higher quality the circle is with more points generated
+    stepSize = 180/n
+    if stepSize < 1:
+        stepSize = 1
+
+    largest_area = 0.0
+    t = 0.0
+    best_h = 0.0
+    base_length = 2 * radius
+    while t<=90: # using degrees
+        radian_t = math.radians(t)
+        h = radius * math.sin(radian_t)
+        tmp_area = 0.5 * base_length * h
+        if tmp_area > largest_area:
+            best_h = h
+        largest_area = max(largest_area, tmp_area)
+        
+        t += stepSize
+    
+    return(best_h)
+
+def largest_triangle_area(radius, h, n):
+    """
+    Returns the largest area of a triangle that can be inscribed inside a
+    circle with the given radius.
+
+    Args:
+      radius: radius of the circle
+      h: the starting height of triangle found after the proof of max area based on diameter as the base of triangle
+      n: gradient
+
+    Returns:
+      Area of the largest inscribed triangle
+    """
+    # The largest triangle is an equilateral triangle
+    largest_A = 0.0
+    step = radius / n
+    if step<0.1:
+        step = 0.1
+    d = 0.0 
+
+    while d < radius:
+        base_length = 2 * math.sqrt(radius**2 - d**2)
+        area = 0.5 * base_length * (d + h)
+        largest_A = max(largest_A, area)
+        d += step
+  
+    return largest_A
+
+# Example usage
+import time
+#radius = 50
+#t0 = time.time()
+#height = largest_triangle_proof(500, radius)
+#max_area = largest_triangle_area(radius, height, 100)
+#t1 = time.time()
+#print(f"The largest area of a triangle inscribed in a circle with radius {radius} is {max_area:.2f} \n Runtime was {t1-t0:.5f}")
